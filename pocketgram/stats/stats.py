976e82ea4568870114f31395baa4707473879772
Crissky <pg2006pe@hotmail.com>
2025-03-06 03:47:03 -0300

    Adiciona slots e acessa get e set dinamicamente Atributos passa a ser protegidos

--- pocketgram/stats/stats.py
+++ pocketgram/stats/stats.py
@@ -7,6 +7,11 @@ class Stats:
     `max_value` é o valor máximo que cada stat pode ter, exibido como "MAX".
     '''
 
+    __slots__ = ('max_value',) + tuple(
+        f'_{stat_enum.name.lower()}'
+        for stat_enum in StatsEnum
+    )
+
     def __init__(
         self,
         hp: int,
@@ -36,15 +41,15 @@ class Stats:
         return sum([self[stats_enum] for stats_enum in StatsEnum])
 
     @property
-    def TOTAL(self) -> int:
-        return int(
-            self.hp +
-            self.attack +
-            self.defense +
-            self.special_attack +
-            self.special_defense +
-            self.speed
-        )
+    def stats_map(self) -> dict:
+        ''' Retorna um dicionário com os nomes dos stats com base no StatsEnum.
+        {<StatsEnum>: _StatsEnum.name.lower}
+        '''
+
+        return {
+            stat_enum: f'_{stat_enum.name.lower()}'
+            for stat_enum in StatsEnum
+        }
 
     def __repr__(self):
         return self.__str__()
@@ -67,19 +72,10 @@ class Stats:
             error_text = f'Chave "{key}" não é do tipo {StatsEnum.__name__}.'
             raise TypeError(error_text)
 
-        if key == StatsEnum.HP:
-            return self.hp
-        elif key == StatsEnum.ATTACK:
-            return self.attack
-        elif key == StatsEnum.DEFENSE:
-            return self.defense
-        elif key in StatsEnum.SPECIAL_ATTACK:
-            return self.special_attack
-        elif key in StatsEnum.SPECIAL_DEFENSE:
-            return self.special_defense
-        elif key == StatsEnum.SPEED:
-            return self.speed
-        else:
+        try:
+            stat_name = self.stats_map[key]
+            return getattr(self, stat_name)
+        except KeyError:
             raise KeyError(f'Chave "{key}" não encontrada.')
 
     def __setitem__(self, key: StatsEnum, value: int):
@@ -89,31 +85,28 @@ class Stats:
             error_text = f'Chave "{key}" não é do tipo {StatsEnum.__name__}.'
             raise TypeError(error_text)
 
-        # Não pode setar um valor maior que o max_value
+        # Validando o range de value
         if value > self.max_value:
             raise ValueError(
                 f'{key.value} não pode ser maior que {self.max_value}.'
             )
-        # Não pode setar um valor negativo
-        if value < 0:
+        elif value < 0:
             raise ValueError(f'{key.value} não pode ser negativo.')
 
-        if key == StatsEnum.HP:
-            self.hp = value
-        elif key == StatsEnum.ATTACK:
-            self.attack = value
-        elif key == StatsEnum.DEFENSE:
-            self.defense = value
-        elif key == StatsEnum.SPECIAL_ATTACK:
-            self.special_attack = value
-        elif key == StatsEnum.SPECIAL_DEFENSE:
-            self.special_defense = value
-        elif key == StatsEnum.SPEED:
-            self.speed = value
-        else:
+        try:
+            stat_name = self.stats_map[key]
+            setattr(self, stat_name, value)
+        except KeyError:
             raise KeyError(f'Chave "{key}" não encontrada.')
 
 
 if __name__ == '__main__':
     stats = Stats(100, 100, 100, 100, 100, 100, 100)
     print(stats)
+    print(stats.stats_map)
+    print('hp:', stats._hp)
+    print('attack:', stats._attack)
+    print('defense:', stats._defense)
+    print('special_attack:', stats._special_attack)
+    print('special_defense:', stats._special_defense)
+    print('speed:', stats._speed)
