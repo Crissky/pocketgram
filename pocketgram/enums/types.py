8e388dc8b7415fd9a2a92bed3dcf753d6cc4b5b5
Crissky <pg2006pe@hotmail.com>
2025-03-08 01:28:11 -0300

    Adiciona TypesEnum

---
+++ pocketgram/enums/types.py
@@ -0,0 +1,43 @@
+from enum import Enum
+
+
+class TypesEnum(Enum):
+    BUG = 'Bug'
+    DARK = 'Dark'
+    DRAGON = 'Dragon'
+    ELECTRIC = 'Electric'
+    FAIRY = 'Fairy'
+    FIGHTING = 'Fighting'
+    FIRE = 'Fire'
+    FLYING = 'Flying'
+    GHOST = 'Ghost'
+    GRASS = 'Grass'
+    GROUND = 'Ground'
+    ICE = 'Ice'
+    NORMAL = 'Normal'
+    POISON = 'Poison'
+    PSYCHIC = 'Psychic'
+    ROCK = 'Rock'
+    STEEL = 'Steel'
+    WATER = 'Water'
+
+
+class TypesEmojiEnum(Enum):
+    BUG = '🐛'
+    DARK = '🌑'
+    DRAGON = '🐲'
+    ELECTRIC = '⚡'
+    FAIRY = '🧚'
+    FIGHTING = '🥊'
+    FIRE = '🔥'
+    FLYING = '🦅'  # 🪽
+    GHOST = '👻'
+    GRASS = '🍃'
+    GROUND = '🟤'
+    ICE = '❄️'
+    NORMAL = '⭐'
+    POISON = '☠️'
+    PSYCHIC = '👁️‍🗨️'  # 🔮🧿🪬👁️
+    ROCK = '🪨'
+    STEEL = '⚙️'
+    WATER = '💧'
