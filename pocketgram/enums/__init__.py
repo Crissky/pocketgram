3d9b1aa723ae91c760cd3af56585388fcf113bf8
Crissky <pg2006pe@hotmail.com>
2025-03-13 00:33:05 -0300

    Refatora types.py para _types.py Conflito com módulo de mesmo nome do Python

--- pocketgram/enums/__init__.py
+++ pocketgram/enums/__init__.py
@@ -1,7 +1,7 @@
 from pocketgram.enums.natures import NaturesEnum  # noqa
 from pocketgram.enums.stats import StatsEnum  # noqa
-from pocketgram.enums.types import TypesEnum  # noqa
-from pocketgram.enums.types import TypesEmojiEnum  # noqa
-from pocketgram.enums.types import TypesSuperEffectiveEnum  # noqa
-from pocketgram.enums.types import TypesNotVeryEffectiveEnum  # noqa
-from pocketgram.enums.types import TypesNoEffectiveEnum  # noqa
+from pocketgram.enums._types import TypesEnum  # noqa
+from pocketgram.enums._types import TypesEmojiEnum  # noqa
+from pocketgram.enums._types import TypesSuperEffectiveEnum  # noqa
+from pocketgram.enums._types import TypesNotVeryEffectiveEnum  # noqa
+from pocketgram.enums._types import TypesNoEffectiveEnum  # noqa
