343e0a722c7e4b43ab4ea2461ad946aa018c67ea
Crissky <pg2006pe@hotmail.com>
2025-03-18 02:54:58 -0300

    WIP: Adicionando BaseMoveEffect

---
+++ pocketgram/move_effect/base_move_effect.py
@@ -0,0 +1,73 @@
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from pocketgram.pocket_monster import PocketMonster
+
+
+class BaseMoveEffect(ABC):
+    '''Classe abstrata que representa a base para as classes dos Efeitos dos
+    Movimentos.
+    '''
+
+    def __init__(self, duration: int = 1):
+        '''
+        Args:
+            duration: Número de turnos que o efeito irá durar.
+        '''
+
+        self.duration = int(duration)
+
+    @abstractmethod
+    def pre_hit_apply(
+        self,
+        target: 'PocketMonster',
+        power: int,
+        total_power: int,
+    ):
+        '''Aplica o efeito no alvo antes do ataque seja realizado.
+
+        Args:
+            target: O alvo em que o efeito será aplicado.
+            power: Poder base do movimento.
+            total_power: Poder total do movimento, incluindo os stats.
+        '''
+
+        pass
+
+    @abstractmethod
+    def hit_apply(
+        self,
+        target: 'PocketMonster',
+        power: int,
+        total_power: int,
+    ):
+        '''Aplica o efeito no alvo se ele foi atingido pelo movimento.
+
+        Args:
+            target: O alvo em que o efeito será aplicado.
+            power: Poder base do movimento.
+            total_power: Poder total do movimento, incluindo os stats.
+        '''
+
+        pass
+
+    @abstractmethod
+    def pos_hit_apply(
+        self,
+        target: 'PocketMonster',
+        damage: int,
+        power: int = None,
+        total_power: int = None,
+    ):
+        '''Aplica o efeito no alvo após o alvo ser atingido.
+
+        Args:
+            target: O alvo em que o efeito será aplicado.
+            damage: Dano causado no pelo ataque.
+            power: Poder base do movimento.
+            total_power: Poder total do movimento, incluindo os stats.
+        '''
+
+        pass
