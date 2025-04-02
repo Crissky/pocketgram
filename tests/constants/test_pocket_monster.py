5151117f90b96a3f959f46f751cfd53e6428fb1b
Crissky <pg2006pe@hotmail.com>
2025-04-02 20:01:37 -0300

    Adiciona test_form

--- tests/constants/test_pocket_monster.py
+++ tests/constants/test_pocket_monster.py
@@ -8,7 +8,7 @@ from pocketgram.enums.stats import StatsEnum
 class TestPocketMonsterDict(unittest.TestCase):
 
     def test_number_keys(self):
-        '''Teste que analisa os números (chaves) do POCKET_MONSTERS_DICT
+        '''Teste que analisa os números (chaves) do POCKET_MONSTERS_DICT.
         '''
 
         length_pocket_monsters = len(POCKET_MONSTERS_DICT.keys())
@@ -16,8 +16,17 @@ class TestPocketMonsterDict(unittest.TestCase):
         for index, key in enumerate(POCKET_MONSTERS_DICT.keys(), start=1):
             self.assertEqual(f'{index:04}', key)
 
+    def test_form(self):
+        '''Teste que analisa os tipos das formas do POCKET_MONSTERS_DICT.
+        '''
+
+        for pocket_monsters_dict in POCKET_MONSTERS_DICT.values():
+            form_types = (str, type(None))
+            for form_key in pocket_monsters_dict.keys():
+                self.assertIsInstance(form_key, form_types)
+
     def test_stats(self):
-        '''Testa se as estatísticas dos monstros são do tipo correta
+        '''Testa se as estatísticas dos monstros são do tipo correta.
         '''
 
         for pocket_monsters_dict in POCKET_MONSTERS_DICT.values():
