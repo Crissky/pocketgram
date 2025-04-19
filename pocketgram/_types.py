5cf98c95c0b5b88704ae8a60395953be0d7b52ef
Crissky <pg2006pe@hotmail.com>
2025-04-18 23:03:48 -0300

    Adiciona checagem de tipo no init e adiciona getitem

--- pocketgram/_types.py
+++ pocketgram/_types.py
@@ -1,4 +1,7 @@
+from typing import Union
+
 from pocketgram.enums._types import TypesEnum
+from pocketgram.enums.pocket_monster import PocketMonsterParamEnum
 
 
 class Types:
@@ -7,9 +10,21 @@ class Types:
 
     def __init__(
         self,
-        type_1: TypesEnum,
-        type_2: TypesEnum = None,
+        type_1: Union[TypesEnum, str],
+        type_2: Union[TypesEnum, str] = None,
     ):
+
+        if type_1 == type_2:
+            type_2 = None
+        if isinstance(type_1, str):
+            type_1 = TypesEnum[type_1]
+        if isinstance(type_2, str):
+            type_2 = TypesEnum[type_2]
+        if not isinstance(type_1, TypesEnum):
+            raise TypeError(f'Tipo "{type_1}" não é válido.')
+        if not isinstance(type_2, (TypesEnum, type(None))):
+            raise TypeError(f'Tipo "{type_2}" não é válido.')
+
         self._type_1 = type_1
         self._type_2 = type_2
 
@@ -26,3 +41,22 @@ class Types:
         '''
 
         return self._type_2
+
+    def __getitem__(self, key: PocketMonsterParamEnum) -> TypesEnum:
+        '''Retorna o tipo do Monstrinho.
+        '''
+
+        if key == PocketMonsterParamEnum.TYPE_1:
+            return self.primary
+        elif key == PocketMonsterParamEnum.TYPE_2:
+            return self.secondary
+        else:
+            raise KeyError(f'Tipo "{key}" não encontrada.')
+
+
+if __name__ == '__main__':
+    types = Types('FIRE', 'FLYING')
+    print(types.primary)
+    print(types.secondary)
+    print(types[PocketMonsterParamEnum.TYPE_1])
+    print(types[PocketMonsterParamEnum.TYPE_2])
