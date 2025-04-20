5a9ba1108dae6ba5a6d6538aaddf3ea8862ab43f
Crissky <pg2006pe@hotmail.com>
2025-04-20 01:14:08 -0300

    Adiciona novos termos para o ENUM

--- pocketgram/enums/pocket_monster.py
+++ pocketgram/enums/pocket_monster.py
@@ -5,7 +5,12 @@ class PocketMonsterParamEnum(Enum):
     '''Enum que representa os Parâmentros da classe dos Monstrinhos.
     '''
 
-    NAME = 'name'
+    FORM = 'form'
     LEVEL = 'level'
+    NAME = 'name'
+    NATURE = 'nature'
+    NICKNAME = 'nickname'
+    NUMBER = 'number'
     TYPE_1 = 'type_1'
     TYPE_2 = 'type_2'
+    TYPES = 'types'
