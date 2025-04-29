8d01f6fb9ee50b2638b48a49253626760c934b66
Crissky <pg2006pe@hotmail.com>
2025-04-28 23:23:35 -0300

    Adiciona TestPocketMonsterFactory

--- tests/factories/test_pocket_monster.py
+++ tests/factories/test_pocket_monster.py
@@ -2,13 +2,14 @@ import unittest
 
 from pocketgram.enums.form import FormEnum
 from pocketgram.enums.natures import NaturesEnum
+from pocketgram.enums.pocket_monster import PocketMonsterParamEnum
+from pocketgram.enums.stats import StatsEnum
 from pocketgram.factories.pocket_monster import pocket_monster_factory
 from pocketgram.pocket_monster import PocketMonster
 
 
 class TestPocketMonsterFactory(unittest.TestCase):
 
-    @unittest.skip("Teste ignorado temporariamente até ser corrigido.")
     def test_pocket_monster_factory_1(self):
         '''Teste se pocket_monster_factory cria uma instância de PocketMonster
         com atributos corretos.
@@ -39,17 +40,35 @@ class TestPocketMonsterFactory(unittest.TestCase):
             ev_special_attack=ev_special_attack,
             ev_special_defense=ev_special_defense,
             ev_speed=ev_speed,
+            iv_random_init=True,
+            nickname='Frieza',
             form=form
         )
 
         self.assertIsInstance(pocket_monster, PocketMonster)
-        self.assertEqual(pocket_monster.number, number)
-        self.assertEqual(pocket_monster.level, level)
-        self.assertEqual(pocket_monster.nature, nature)
-        self.assertEqual(pocket_monster.ev_hp, ev_hp)
-        self.assertEqual(pocket_monster.ev_attack, ev_attack)
-        self.assertEqual(pocket_monster.ev_defense, ev_defense)
-        self.assertEqual(pocket_monster.ev_special_attack, ev_special_attack)
-        self.assertEqual(pocket_monster.ev_special_defense, ev_special_defense)
-        self.assertEqual(pocket_monster.ev_speed, ev_speed)
-        self.assertEqual(pocket_monster.form, form)
+        self.assertEqual(
+            pocket_monster[PocketMonsterParamEnum.NUMBER], f'{number:04}'
+        )
+        self.assertEqual(pocket_monster[PocketMonsterParamEnum.LEVEL], level)
+        self.assertEqual(pocket_monster[PocketMonsterParamEnum.NATURE], nature)
+        self.assertEqual(
+            pocket_monster._ev_stats[StatsEnum.HP], ev_hp
+        )
+        self.assertEqual(
+            pocket_monster._ev_stats[StatsEnum.ATTACK], ev_attack
+        )
+        self.assertEqual(
+            pocket_monster._ev_stats[StatsEnum.DEFENSE], ev_defense
+        )
+        self.assertEqual(
+            pocket_monster._ev_stats[StatsEnum.SPECIAL_ATTACK],
+            ev_special_attack
+        )
+        self.assertEqual(
+            pocket_monster._ev_stats[StatsEnum.SPECIAL_DEFENSE],
+            ev_special_defense
+        )
+        self.assertEqual(
+            pocket_monster._ev_stats[StatsEnum.SPEED], ev_speed
+        )
+        self.assertEqual(pocket_monster[PocketMonsterParamEnum.FORM], form)
