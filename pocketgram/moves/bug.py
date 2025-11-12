dbd1d6880146bcf99abdf98eabbd1481e98028af
Crissky <pg2006pe@hotmail.com>
2025-11-12 18:35:37 -0300

    WIP: Adiciona Bug moves

---
+++ pocketgram/moves/bug.py
@@ -0,0 +1,33 @@
+from pocketgram.enums._types import TypesEnum
+from pocketgram.enums.move_category import MoveCategoryEnum
+from pocketgram.enums.move_name import MoveNameEnum
+from pocketgram.moves.base_move import BaseMove
+
+
+# GEN 1
+class leech_life(BaseMove):
+    def __init__(self, used_pp: int):
+        super().__init__(
+            name=MoveNameEnum.LEECH_LIFE.value,
+            _type=TypesEnum.BUG,
+            category=MoveCategoryEnum.PHYSICAL,
+            used_pp=used_pp,
+            max_pp=10,
+            power=80,
+            accuracy=100,
+            priority=0,
+            effect_list=None,
+            makes_contact=True,
+        )
+
+
+class pin_missile(BaseMove):
+    ...
+
+
+class string_shot(BaseMove):
+    ...
+
+
+class twineedle(BaseMove):
+    ...
