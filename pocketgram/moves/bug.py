3b823583f173d8e1f855aeb7dfe47ec3d6656fb5
Crissky <pg2006pe@hotmail.com>
2025-11-14 18:30:35 -0300

    Adiciona descrição de Leech Life

--- pocketgram/moves/bug.py
+++ pocketgram/moves/bug.py
@@ -7,8 +7,11 @@ from pocketgram.moves.base_move import BaseMove
 # GEN 1
 class leech_life(BaseMove):
     def __init__(self, used_pp: int):
+        description = 'Steals 1/2 of the damage inflicted.'
+
         super().__init__(
             name=MoveNameEnum.LEECH_LIFE.value,
+            description=description,
             _type=TypesEnum.BUG,
             category=MoveCategoryEnum.PHYSICAL,
             used_pp=used_pp,
