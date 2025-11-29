701f983c990dc8b0ca4cab48bf66fe1314385734
Crissky <pg2006pe@hotmail.com>
2025-11-29 09:05:27 -0300

    WIP: Adicionando Electric Moves

---
+++ pocketgram/moves/electric.py
@@ -0,0 +1,27 @@
+from pocketgram.enums._types import TypesEnum
+from pocketgram.enums.move_category import MoveCategoryEnum
+from pocketgram.enums.move_name import MoveNameEnum
+from pocketgram.moves.base_move import BaseMove
+
+
+# =============================================================================
+# GEN 1
+# =============================================================================
+class thunder(BaseMove):
+    ...
+
+
+class thunder_punch(BaseMove):
+    ...
+
+
+class thunder_shock(BaseMove):
+    ...
+
+
+class thunder_wave(BaseMove):
+    ...
+
+
+class thunderbolt(BaseMove):
+    ...
