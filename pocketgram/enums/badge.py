107488ab225744c70f311c05d706a984521b23ab
Crissky <pg2006pe@hotmail.com>
2025-12-01 19:57:39 -0300

    Adiciona BadgeEnum

---
+++ pocketgram/enums/badge.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class BadgeEnum(Enum):
+    ...
+
+
+class KantoBadgeEnum(BadgeEnum):
+    BOULDER = 'ROCHA'
+    CASCADE = 'CASCATA'
+    THUNDER = 'TROVÃO'
+    RAINBOW = 'ARCO-ÍRIS'
+    SOUL = 'ALMA'
+    MARSH = 'LAMA'
+    VOLCANO = 'VULCÃO'
+    EARTH = 'TERRA'
