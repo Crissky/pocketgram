726e14bc4fdecc33ee90fb575c18feff78f5d923
Crissky <pg2006pe@hotmail.com>
2025-12-02 19:23:33 -0300

    Adiciona Thunder

--- pocketgram/moves/electric.py
+++ pocketgram/moves/electric.py
@@ -8,7 +8,22 @@ from pocketgram.moves.base_move import BaseMove
 # GEN 1
 # =============================================================================
 class thunder(BaseMove):
-    ...
+    def __init__(self, used_pp: int):
+        description = 'An attack that may cause paralysis.'
+
+        super().__init__(
+            name=MoveNameEnum.THUNDER.value,
+            description=description,
+            _type=TypesEnum.ELECTRIC,
+            category=MoveCategoryEnum.SPECIAL,
+            used_pp=used_pp,
+            max_pp=10,
+            power=110,
+            accuracy=70,
+            priority=0,
+            effect_list=None,
+            makes_contact=False,
+        )
 
 
 class thunder_punch(BaseMove):
