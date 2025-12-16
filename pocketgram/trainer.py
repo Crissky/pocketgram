5cd50031e561986eaf088776b575ac2c14afae36
Crissky <pg2006pe@hotmail.com>
2025-12-16 01:49:35 -0300

    Refatora para usar MongoBase

--- pocketgram/trainer.py
+++ pocketgram/trainer.py
@@ -1,19 +1,13 @@
+from dataclasses import dataclass, field
+from typing import List
+
 from pocketgram.enums.badge import BadgeEnum
+from repository.mongo.base import MongoBase
 
 
-class Trainer:
+@dataclass
+class Trainer(MongoBase):
 
-    def __init__(
-        self,
-        _id: str,
-        user_id: str,
-        user_name: str,
-        badge_list: list[BadgeEnum] = None,
-    ):
-        if badge_list is None:
-            badge_list = []
-
-        self._id = _id
-        self.user_id = user_id
-        self.user_name = user_name
-        self.badge_list = badge_list
+    user_id: str
+    user_name: str
+    badge_list: List[BadgeEnum] = field(default_factory=list)
