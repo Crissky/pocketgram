688a87d1aaea6407482086e775612fbc68aa12ed
Crissky <pg2006pe@hotmail.com>
2025-12-18 23:38:14 -0300

    Adiciona verificação de badge_list em post_init

--- pocketgram/trainer.py
+++ pocketgram/trainer.py
@@ -10,4 +10,18 @@ class Trainer(MongoBase):
 
     user_id: str
     user_name: str
-    badge_list: List[BadgeEnum] = field(default_factory=list)
+    badge_list: List[Union[BadgeEnum, str]] = field(default_factory=list)
+
+    def __post_init__(self):
+        super().__post_init__()
+
+        for index, bagde_enum in enumerate(self.badge_list):
+            if isinstance(bagde_enum, str):
+                self.badge_list[index] = BadgeEnum(bagde_enum)
+
+            if not isinstance(self.badge_list[index], BadgeEnum):
+                raise TypeError(
+                    f"O badge_list[{index}] não é um BadgeEnum válido. "
+                    f"Tipo: {type(bagde_enum)}"
+                )
+
