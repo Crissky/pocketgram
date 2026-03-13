014a2efbabbd1528d6a2c9584b2bad4d6a8cb92b
Crissky <pg2006pe@hotmail.com>
2026-03-12 23:29:43 -0300

    Adiciona from_name

--- pocketgram/enums/badge.py
+++ pocketgram/enums/badge.py
@@ -2,7 +2,14 @@ from enum import Enum
 
 
 class BadgeEnum(Enum):
-    ...
+    @classmethod
+    def from_name(cls, name: str):
+        for subclass in cls.__subclasses__():
+            try:
+                return subclass[name]
+            except KeyError:
+                continue
+        raise ValueError(f"Badge '{name}' não encontrada")
 
 
 class KantoBadgeEnum(BadgeEnum):
