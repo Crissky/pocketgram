54c6fbae0c18263e8a82975509d5ae59d38d87d6
Crissky <pg2006pe@hotmail.com>
2026-03-12 23:27:58 -0300

    Adiciona logging

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 '''
 
 
+import logging
+
 from decouple import config
 
 from telegram.ext import Application
@@ -13,9 +15,27 @@ TELEGRAM_TOKEN = config("TELEGRAM_TOKEN")
 MY_GROUP_ID = config('MY_GROUP_ID', cast=int)
 ENVIRONMENT = config('ENVIRONMENT', default='PRODUCTION')
 
+# Logging Config
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+logger = logging.getLogger(__name__)
+
 
 def main() -> None:
     """Run the bot."""
+    # Logging
+    if TELEGRAM_TOKEN is None:
+        logger.warning('Variável TELEGRAM_TOKEN não definida.')
+    else:
+        logger.info('Variável TELEGRAM_TOKEN definida.')
+    if MY_GROUP_ID is None:
+        logger.warning('Variável MY_GROUP_ID não definida.')
+    else:
+        logger.info('Variável MY_GROUP_ID definida.')
+    logger.info(f'Environment: {ENVIRONMENT}')
 
     # Create the Application and pass it your bot's token.
     application = Application.builder().token(TELEGRAM_TOKEN).build()
