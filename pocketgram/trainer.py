58140b68efa840b780c26c888e04df1d2c28d3ed
Crissky <pg2006pe@hotmail.com>
2026-03-14 22:49:24 -0300

    Corrige chamada de BagdeEnum

--- pocketgram/trainer.py
+++ pocketgram/trainer.py
@@ -21,14 +21,14 @@ class Trainer(MongoBase):
         if self.user_name is not None:
             self.user_name = str(self.user_name)
 
-        for index, bagde_enum in enumerate(self.badge_list):
-            if isinstance(bagde_enum, str):
-                self.badge_list[index] = BadgeEnum(bagde_enum)
+        for index, bagde_str in enumerate(self.badge_list):
+            if isinstance(bagde_str, str):
+                self.badge_list[index] = BadgeEnum.from_name(bagde_str)
 
             if not isinstance(self.badge_list[index], BadgeEnum):
                 raise TypeError(
                     f"O badge_list[{index}] não é um BadgeEnum válido. "
-                    f"Tipo: {type(bagde_enum)}"
+                    f"Tipo: {type(bagde_str)}"
                 )
 
     def __str__(self):
