96d6989cf0c489699b60e604e335eedefaea7f18
Crissky <pg2006pe@hotmail.com>
2026-04-01 10:50:51 -0300

    Adiciona GenderEnum

---
+++ pocketgram/enums/gender.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class GenderEnum(Enum):
+    """Enum que representa os Gêneros dos Monstrinhos."""
+
+    MALE = "Male"
+    FEMALE = "Female"
+    GENDERLESS = "Genderless"
