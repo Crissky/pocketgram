8707f885db40431f7f225d55610a902f1148defe
Crissky <pg2006pe@hotmail.com>
2026-04-02 23:45:38 -0300

    Adiciona LanguagesEnum

---
+++ pocketgram/enums/languages.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class LanguagesEnum(Enum):
+    """Enum que representa os idiomas disponíveis."""
+
+    ENGLISH = "english"
+    JAPANESE = "japanese"
+    GERMAN = "german"
+    FRENCH = "french"
+    ITALIAN = "italian"
+    SPANISH = "spanish"
+    KOREAN = "korean"
+    CHINESE_SIMPLIFIED = "chinese simplified"
+    CHINESE_TRADITIONAL = "chinese traditional"
