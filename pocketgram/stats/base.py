1dbb5b94b84dfec24f127183c04a595e16e8ab8f
Crissky <pg2006pe@hotmail.com>
2026-04-10 23:45:08 -0300

    Refatora BaseStats para init com PocketMonster

--- pocketgram/stats/base.py
+++ pocketgram/stats/base.py
@@ -1,30 +1,33 @@
+from typing import TYPE_CHECKING
+
 from pocketgram.stats.stats import Stats
 
 
-class BaseStats(Stats):
-    '''Valores base dos stats que compõem o valor final dos Stats.
-    '''
+if TYPE_CHECKING:
+    from pocketgram.pocket_monster import PocketMonster
 
-    def __init__(
-        self,
-        hp: int,
-        attack: int,
-        defense: int,
-        special_attack: int,
-        special_defense: int,
-        speed: int,
-    ):
+
+class BaseStats(Stats):
+    """Valores base dos stats que compõem o valor final dos Stats."""
+
+    def __init__(self, pocket_monster: "PocketMonster"):
         super().__init__(
-            hp=hp,
-            attack=attack,
-            defense=defense,
-            special_attack=special_attack,
-            special_defense=special_defense,
-            speed=speed,
-            max_value=255
+            pocket_monster=pocket_monster, max_value=255, prefix="base_"
         )
 
 
-if __name__ == '__main__':
-    stats = BaseStats(35, 55, 40, 50, 50, 90)
-    print(stats)
+if __name__ == "__main__":
+    from types import SimpleNamespace
+    from pocketgram.enums.stats import StatsEnum
+
+    pm = SimpleNamespace(
+        **{
+            f"base_{s.name.lower()}": n
+            for n, s in enumerate(StatsEnum, start=1)
+        }
+    )
+    base_stats = BaseStats(pocket_monster=pm)
+    for s in StatsEnum:
+        print(s, base_stats[s])
+
+    print(base_stats)
