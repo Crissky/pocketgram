6545e0468389c6d52ac4ec80d8681fdde8dfc9f5
Crissky <pg2006pe@hotmail.com>
2026-04-13 23:58:16 -0300

    Refatora IVStats para init com PocketMonster

--- pocketgram/stats/iv.py
+++ pocketgram/stats/iv.py
@@ -1,7 +1,13 @@
 
-from random import randint
+from random import randint, choices
+from typing import TYPE_CHECKING
 
+from pocketgram.enums.stats import StatsEnum
 from pocketgram.stats.stats import Stats
+
+
+if TYPE_CHECKING:
+    from pocketgram.pocket_monster import PocketMonster
 
 
 class IVStats(Stats):
@@ -11,28 +17,28 @@ class IVStats(Stats):
 
     def __init__(
         self,
-        hp: int = None,
-        attack: int = None,
-        defense: int = None,
-        special_attack: int = None,
-        special_defense: int = None,
-        speed: int = None,
+        pocket_monster: "PocketMonster",
         random_init: bool = False
     ):
+        super().__init__(
+            pocket_monster=pocket_monster,
+            max_value=31,
+            prefix='iv_'
+        )
         if random_init is True:
             init_error = ''
-            if hp is not None:
-                init_error += f'hp={hp}, '
-            if attack is not None:
-                init_error += f'attack={attack}, '
-            if defense is not None:
-                init_error += f'defense={defense}, '
-            if special_attack is not None:
-                init_error += f'special_attack={special_attack}, '
-            if special_defense is not None:
-                init_error += f'special_defense={special_defense}, '
-            if speed is not None:
-                init_error += f'speed={speed}, '
+            if self[StatsEnum.HP] is not None:
+                init_error += f'hp={self[StatsEnum.HP]}, '
+            if self[StatsEnum.ATTACK] is not None:
+                init_error += f'attack={self[StatsEnum.ATTACK]}, '
+            if self[StatsEnum.DEFENSE] is not None:
+                init_error += f'defense={self[StatsEnum.DEFENSE]}, '
+            if self[StatsEnum.SPECIAL_ATTACK] is not None:
+                init_error += f'special_attack={self[StatsEnum.SPECIAL_ATTACK]}, '
+            if self[StatsEnum.SPECIAL_DEFENSE] is not None:
+                init_error += f'special_defense={self[StatsEnum.SPECIAL_DEFENSE]}, '
+            if self[StatsEnum.SPEED] is not None:
+                init_error += f'speed={self[StatsEnum.SPEED]}, '
 
             if init_error != '':
                 init_error = init_error[:-2]
@@ -41,42 +47,61 @@ class IVStats(Stats):
                     f'random_init: {init_error}.'
                 )
 
-            hp = self.get_random_iv()
-            attack = self.get_random_iv()
-            defense = self.get_random_iv()
-            special_attack = self.get_random_iv()
-            special_defense = self.get_random_iv()
-            speed = self.get_random_iv()
+            self[StatsEnum.HP] = self.get_random_iv()
+            self[StatsEnum.ATTACK] = self.get_random_iv()
+            self[StatsEnum.DEFENSE] = self.get_random_iv()
+            self[StatsEnum.SPECIAL_ATTACK] = self.get_random_iv()
+            self[StatsEnum.SPECIAL_DEFENSE] = self.get_random_iv()
+            self[StatsEnum.SPEED] = self.get_random_iv()
 
-        if hp is None:
-            raise ValueError('hp não foi definido.')
-        if attack is None:
-            raise ValueError('attack não foi definido.')
-        if defense is None:
-            raise ValueError('defense não foi definido.')
-        if special_attack is None:
-            raise ValueError('special_attack não foi definido.')
-        if special_defense is None:
-            raise ValueError('special_defense não foi definido.')
-        if speed is None:
-            raise ValueError('speed não foi definido.')
-
-        super().__init__(
-            hp=hp,
-            attack=attack,
-            defense=defense,
-            special_attack=special_attack,
-            special_defense=special_defense,
-            speed=speed,
-            max_value=31
-        )
+        init_stats_error = ''
+        if self[StatsEnum.HP] is None:
+            init_stats_error += 'hp, '
+        if self[StatsEnum.ATTACK] is None:
+            init_stats_error += 'attack, '
+        if self[StatsEnum.DEFENSE] is None:
+            init_stats_error += 'defense, '
+        if self[StatsEnum.SPECIAL_ATTACK] is None:
+            init_stats_error += 'special_attack, '
+        if self[StatsEnum.SPECIAL_DEFENSE] is None:
+            init_stats_error += 'special_defense, '
+        if self[StatsEnum.SPEED] is None:
+            init_stats_error += 'speed, '
+        if init_stats_error != '':
+            raise ValueError(
+                f'Os seguintes parâmetros não foram definidos: '
+                f'{init_stats_error[:-2]}.'
+            )
 
     def get_random_iv(self):
-        return randint(0, 31)
+        values = list(range(32))
+        weights = [i+v for i, v in enumerate(values, start=1)]
+
+        return choices(values, weights=weights)[0]
 
 
 if __name__ == '__main__':
-    stats = IVStats(31, 0, 31, 31, 31, 31)
-    print(stats)
-    stats = IVStats(random_init=True)
-    print(stats)
+    from types import SimpleNamespace
+    pm = SimpleNamespace(
+        **{
+            f"iv_{s.name.lower()}": n*6
+            for n, s in enumerate(StatsEnum, start=1)
+        }
+    )
+    pm_none = SimpleNamespace(
+        **{
+            f"iv_{s.name.lower()}": None for s in StatsEnum
+        }
+    )
+
+    iv_stats = IVStats(pocket_monster=pm)
+    for s in StatsEnum:
+        print(s, iv_stats[s])
+
+    print(iv_stats)
+
+    iv_stats_none = IVStats(pocket_monster=pm_none, random_init=True)
+    for s in StatsEnum:
+        print(s, iv_stats_none[s])
+
+    print(iv_stats_none)
