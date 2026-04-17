09e285495252f90f0a487435766eb8b6ec220b6b
Crissky <pg2006pe@hotmail.com>
2026-04-16 23:47:35 -0300

    Adiciona imports

--- pocketgram/stats/__init__.py
+++ pocketgram/stats/__init__.py
@@ -1,3 +1,6 @@
+from pocketgram.stats.base import BaseStats  # noqa
 from pocketgram.stats.ev import EVStats  # noqa
 from pocketgram.stats.iv import IVStats  # noqa
+from pocketgram.stats.nature import Nature  # noqa
+from pocketgram.stats.stage import StageStats  # noqa
 from pocketgram.stats.stats import Stats  # noqa
