33de932a0cb6d2618d3195ebf3e6d353dfe87176
Crissky <pg2006pe@hotmail.com>
2026-04-21 04:01:22 -0300

    Corrige testes

--- tests/pocketgram/stats/test_base.py
+++ tests/pocketgram/stats/test_base.py
@@ -1,5 +1,7 @@
 import unittest
 
+from types import SimpleNamespace
+
 from pocketgram.enums.stats import StatsEnum
 from pocketgram.stats.base import BaseStats
 
@@ -10,10 +12,11 @@ class TestBase(unittest.TestCase):
         '''Teste que verifica que se inicializa corretamente o objeto BaseStats
         '''
 
-        base_stats = BaseStats(
-            hp=35, attack=55, defense=40,
-            special_attack=50, special_defense=50, speed=90
+        pocket_monster = SimpleNamespace(
+            base_hp=35, base_attack=55, base_defense=40,
+            base_special_attack=50, base_special_defense=50, base_speed=90
         )
+        base_stats = BaseStats(pocket_monster=pocket_monster)
 
         self.assertEqual(base_stats[StatsEnum.HP], 35)
         self.assertEqual(base_stats[StatsEnum.ATTACK], 55)
@@ -27,19 +30,27 @@ class TestBase(unittest.TestCase):
         levanta uma excessão.
         '''
 
-        with self.assertRaises(ValueError):
-            BaseStats(
-                hp=-1, attack=0, defense=0,
-                special_attack=0, special_defense=0, speed=0
+        expected = 'HP=-1 > MIN=0'
+        with self.assertRaises(ValueError) as context:
+            pocket_monster = SimpleNamespace(
+                base_hp=-1, base_attack=0, base_defense=0,
+                base_special_attack=0, base_special_defense=0, base_speed=0
             )
+            BaseStats(pocket_monster=pocket_monster)
+
+        self.assertIn(expected, str(context.exception))
 
     def test_init_out_of_bounds_values(self):
         '''Teste que inicializa BaseStats com valores fora dos limites
         permitidos (255).
         '''
 
-        with self.assertRaises(ValueError):
-            BaseStats(
-                hp=256, attack=0, defense=0,
-                special_attack=0, special_defense=0, speed=0
+        expected = 'HP=256 > MAX=255'
+        with self.assertRaises(ValueError) as context:
+            pocket_monster = SimpleNamespace(
+                base_hp=256, base_attack=0, base_defense=0,
+                base_special_attack=0, base_special_defense=0, base_speed=0
             )
+            BaseStats(pocket_monster=pocket_monster)
+
+        self.assertIn(expected, str(context.exception))
